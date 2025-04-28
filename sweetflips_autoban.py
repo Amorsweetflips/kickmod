a3237ffc8f83e0b3e67b7900b61d20e562312bbf
Amorsweetflips <MCserverpitufo@gmail.com>
2025-04-28 15:59:33 +0200

    Update sweetflips_autoban.py

--- sweetflips_autoban.py
+++ sweetflips_autoban.py
@@ -19,7 +19,8 @@ DUPLICATE_TIME_WINDOW = 5  # seconds
 options = uc.ChromeOptions()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=VizDisplayCompositor")
 options.add_argument("--headless=new")
 options.binary_location = "/usr/bin/chromium"
 
